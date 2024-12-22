54811f412f0dadfb9a8454bc46dc81e0e865e2ba
Abhiram Kanna Seelamanthula <88733940+aksisonline@users.noreply.github.com>
2024-12-23 02:10:21 +0530

    Added Manifest

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Active Chat',
+    short_name: 'Active Chat',
+    description: 'Secure, Anonymous, Serverless messaging for your peace of mind.',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#000000',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: '/ac_logo_white.png',
+        sizes: 'any',
+        type: 'image/png',
+      },
+    ],
+  }
+}
